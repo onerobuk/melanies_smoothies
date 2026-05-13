0e8d4afd4b54996822e0cfb4daba8ecc519ef6b7
onerobuk <153583332+onerobuk@users.noreply.github.com>
2026-05-13 00:37:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write("The name for your order is ", name_on_order)
 
 cnx = st.connection('snowflake')
 session=cnx.session()
-my_dataframe=session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 ingredients_list = st.multiselect(
