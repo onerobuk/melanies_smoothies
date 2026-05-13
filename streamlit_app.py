14aca00f1fafb75dce99f7cee0271429aba5ec3a
onerobuk <153583332+onerobuk@users.noreply.github.com>
2026-05-12 23:53:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages.
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app.
@@ -33,3 +34,6 @@ if ingredients_list:
     if ingredients_string and time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+smoothiefruit_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefruit_response)
